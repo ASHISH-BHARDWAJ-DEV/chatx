76eb3be08dcfa4f24d2bc0065b36134c969742dd
ashish_3zz <ashishbhardwaj456789@gmail.com>
2025-12-15 18:43:04 +0530

    reviewed signup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,30 +1,28 @@
-import dotenv from "dotenv";
-dotenv.config();
 import express from "express";
 import path from "path";
 import authRoutes from "./routes/auth.route.js";
-import  messageRoutes from "./routes/message.route.js";
+import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+
 app.use(express.json());
+
 const __dirname = path.resolve();
 
-app.use("/api/auth",authRoutes);
-app.use("/api/messages",messageRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
-if(process.env.NODE_ENV === "production"){
-    app.use(express.static(path.join( __dirname ,"../frontend/dist")));
-    app.get("*",(_,res) => {
-        res.sendFile(path.join(__dirname, "../frontend","dist","index.html"));
-    });
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
 }
 
-
-
 connectDB();
+
 app.listen(PORT, () => {
   console.log("Server running on port " + PORT);
-  
 });
-
